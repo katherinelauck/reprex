cde0ee62802603743d2ba517c18aa05ae0a056ca
Katherine Lauck <kslauck@ucdavis.edu>
2024-07-08 13:03:00 -0700

    full reprex

---
+++ reprex.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --partition=med2
+#SBATCH --mem=2000
+#SBATCH --time=00:04:00
+#SBATCH --cpus-per-task=1
+#SBATCH -D /group/dkarpgrp/kslauck/reprex
+#SBATCH --output=test_job.out
+#SBATCH --error=test_job.err
+
+echo "Partition: $SLURM_JOB_PARTITION"
+
+snakemake --unlock --snakefile reprex
+
+snakemake --profile .
+
+# Print out values of the current jobs SLURM environment variables
+env | grep SLURM
+
+# Print out final statistics about resource use before job exits
+scontrol show job ${SLURM_JOB_ID}
+
+sstat --format 'JobID,MaxRSS,AveCPU' -P ${SLURM_JOB_ID}.batch
